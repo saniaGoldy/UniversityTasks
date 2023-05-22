0c1b73882ec1f45b052ac90939a3cffccb003756
Olha Korchysta <olhak@fivesysdev.com>
2023-05-22 14:45:49 +0300

    Zoo with array of animals, basic ui in terminal implemented

--- src/Main.kt
+++ src/Main.kt
@@ -1,80 +1,124 @@
-import example.testZoo
-import model.Animal
+import kotlin.math.pow
 
-enum class MenuOptions(val optionId: Int){
-    ZooInfo(1),
-    AddAnAnimal(2),
-    DeleteAnimal(3),
-    SortAnimalsByName(4),
-    SortAnimalsByWeight(5),
-    SortBySpecie(6),
-    SortByPreferredFood(7),
-    Exit(0)
-}
+class Matrix(val elements: Array<Array<Int>>) {
 
-fun main() {
+    fun getInverseMatrix(): Matrix {
+        val size = elements.size
+        val cofactors = Array(size) { Array(size) { 0 } }
 
-    while (true) {
-
-        MenuOptions.values().forEach {
-            println("print ${it.optionId} to $it")
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                val minor = getMinorMatrix(i, j)
+                val sign = (-1).toDouble().pow(i + j).toInt()
+                cofactors[j][i] = sign * minor.getDeterminant()
+            }
         }
 
-        var input: Int
-        try {
-            input = readLine()!!.toInt()
+        val determinant = getDeterminant()
+        val inverseElements = Array(size) { Array(size) { 0 } }
 
-            val selectedOption = MenuOptions.values().first { it.optionId == input }
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                inverseElements[i][j] = cofactors[i][j] / determinant
+            }
+        }
 
-            when(selectedOption){
-                MenuOptions.ZooInfo -> {
-                    print(testZoo)
-                }
-                MenuOptions.AddAnAnimal -> {
-                    testZoo.addAnimals(readAnimal())
-                }
-                MenuOptions.DeleteAnimal -> {
-                    print("input animal name")
-                    testZoo.removeAnimals(readln())
-                }
-                MenuOptions.SortAnimalsByName -> {
-                    print(testZoo.getAnimalsSortedByName())
-                }
-                MenuOptions.SortByPreferredFood -> {
-                    print(testZoo.getAnimalsSortedByPreferredFood())
-                }
-                MenuOptions.SortBySpecie ->{
-                    print(testZoo.getAnimalsSortedBySpecie())
-                }
-                MenuOptions.SortAnimalsByWeight -> {
-                    println("input 1 to sort in ascending order, otherwise any key")
-                    val choise = readLine()
-                    print(if (choise == "1")
-                        testZoo.getAnimalsSortedByWeight(true)
-                    else
-                        testZoo.getAnimalsSortedByWeight(false)
-                    )
-                }
-                MenuOptions.Exit -> break
+        return Matrix(inverseElements)
+    }
+
+    fun getDeterminant(): Int {
+        val size = elements.size
+
+        if (size == 1) {
+            return elements[0][0]
+        }
+
+        var determinant = 0
+
+        for (i in 0 until size) {
+            val minor = getMinorMatrix(0, i)
+            val sign = (-1).toDouble().pow(i).toInt()
+            determinant += sign * elements[0][i] * minor.getDeterminant()
+        }
+
+        return determinant
+    }
+
+    private fun getMinorMatrix(row: Int, column: Int): Matrix {
+        val size = elements.size
+        val minorElements = Array(size - 1) { Array(size - 1) { 0 } }
+        var minorRow = 0
+
+        for (i in 0 until size) {
+            if (i == row) continue
+            var minorColumn = 0
+
+            for (j in 0 until size) {
+                if (j == column) continue
+                minorElements[minorRow][minorColumn] = elements[i][j]
+                minorColumn++
             }
 
-            println()
-        } catch (e: Exception) {
-            print("Invalid input, please enter option number")
-            continue
+            minorRow++
         }
+
+        return Matrix(minorElements)
+    }
+
+    fun multiplyByInverse(): Matrix {
+        val inverseMatrix = getInverseMatrix()
+        return multiplyByMatrix(inverseMatrix)
+    }
+
+    private fun multiplyByMatrix(matrix: Matrix): Matrix {
+        val size = elements.size
+        val resultElements = Array(size) { Array(size) { 0 } }
+
+        for (i in 0 until size) {
+            for (j in 0 until size) {
+                for (k in 0 until size) {
+                    resultElements[i][j] += elements[i][k] * matrix.elements[k][j]
+                }
+            }
+        }
+
+        return Matrix(resultElements)
     }
 }
 
-private fun readAnimal(): Animal {
-    print("input animal specie:")
-    val specie = readln()
-    print("input animal name")
-    val name = readln()
-    print("input animal weight")
-    val weight = readln().toFloat()
-    print("input animal preferredFood")
-    val preferredFood = readln()
+fun main() {
+    val elements = arrayOf(
+        arrayOf(415, 747, 186, 663, 415, 709, 954, 670, 993, 718),
+        arrayOf(969, 644, 835, 470, 876, 4, 670, 997, 651, 872),
+        arrayOf(77, 10, 340, 459, 79, 580, 437, 195, 111, 828),
+        arrayOf(628, 160, 433, 231, 303, 701, 297, 195, 246, 336),
+        arrayOf(695, 600, 156, 434, 787, 724, 874, 855, 256, 558),
+        arrayOf(665, 125, 369, 480, 11, 749, 105, 514, 314, 694),
+        arrayOf(324, 498, 66, 445, 973, 418, 647, 314, 561, 129),
+        arrayOf(947, 181, 851, 580, 714, 599, 559, 87, 45, 633),
+        arrayOf(421, 625, 801, 861, 200, 415, 194, 337, 165, 396),
+        arrayOf(341, 425, 377, 862, 197, 25, 371, 834, 128, 431)
+    )
 
-    return Animal(specie, name, weight, preferredFood)
+    val matrix = Matrix(elements)
+
+    val inverseMatrix = matrix.getInverseMatrix()
+    println("Inverse Matrix:")
+    inverseMatrix.printMatrix()
+
+    val determinant = matrix.getDeterminant()
+    println("Determinant: $determinant")
+
+    val multipliedMatrix = matrix.multiplyByInverse()
+    println("Multiplied Matrix:")
+    multipliedMatrix.printMatrix()
+}
+
+fun Matrix.printMatrix() {
+    for (row in elements) {
+        for (element in row) {
+            print("$element ")
+        }
+        println()
+    }
 }
--- src/model/Animal.kt
+++
@@ -1,8 +0,0 @@
-package model
-
-data class Animal(val specie: String,val name: String, val weight: Float, val preferredFood: String){
-
-    override fun toString(): String {
-        return "\nAnimal info:\nspecie: $specie\nname: $name\nweight: $weight\npreferredFood: $preferredFood\n"
-    }
-}
